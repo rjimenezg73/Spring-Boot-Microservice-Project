a2726d9f1ce5d867ee2c52b9a0a2702d0dd4f128
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-07 23:45:43 -0600

    first commit Spring MongoDB Tutorial

---
+++ spring-mongodb-tutorial/src/main/java/com/programmingspring/SpringMongodbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.programmingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbTutorialApplication.class, args);
+	}
+
+}
---
+++ spring-mongodb-tutorial/src/test/java/com/programmingspring/SpringMongodbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
