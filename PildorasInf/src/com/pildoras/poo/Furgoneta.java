9e19a79628fd21702cccbbcbec6deec3f1dc81ac
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-14 22:54:53 -0600

    Herencia 2 - super()

--- PildorasInf/src/com/pildoras/poo/Furgoneta.java
+++ PildorasInf/src/com/pildoras/poo/Furgoneta.java
@@ -2,12 +2,31 @@ package com.pildoras.poo;
 
 public class Furgoneta extends Coche{
    private int capacidad_carga;
-   private int plazas_extras;
+   private int plazas_extra;
 
-   public Furgoneta(int capacidad_carga, int plazas_extras){
+   public Furgoneta(int capacidad_carga, int plazas_extra){
       super(); // Llama al constructor de la clase padre
       this.capacidad_carga = capacidad_carga;
-      this.plazas_extras = plazas_extras;
+      this.plazas_extra = plazas_extra;
+   }
 
+   public int getCapacidad_carga() {
+      return capacidad_carga;
+   }
+
+   public void setCapacidad_carga(int capacidad_carga) {
+      this.capacidad_carga = capacidad_carga;
+   }
+
+   public int getPlazas_extra() {
+      return plazas_extra;
+   }
+
+   public void setPlazas_extra(int plazas_extra) {
+      this.plazas_extra = plazas_extra;
+   }
+
+   public String dameDatosFurgoneta(){
+      return "La capacidad de carga es: " + this.getCapacidad_carga() + " Y las plazas son: " + this.getPlazas_extra();
    }
 }
--- PildorasInf/src/com/pildoras/poo/Uso_Vehiculo.java
+++ PildorasInf/src/com/pildoras/poo/Uso_Vehiculo.java
@@ -1,11 +1,19 @@
 package com.pildoras.poo;
 
-public class Uso_Coche {
+public class Uso_Vehiculo {
    public static void main(String[] args) {
       Coche renault = new Coche();
       //renault.ruedas = 3;
       System.out.println("Éste coche tiene " + renault.getRuedas() + " ruedas.");
       renault.setColor("Blanco");
       System.out.println("El color del coche es " + renault.getColor());
+
+      // Objeto Furgoneta
+      Coche miCoche1 = new Coche();
+      miCoche1.setColor("Rojo");
+
+      Furgoneta miFurgoneta1 = new Furgoneta(580, 7);
+      miFurgoneta1.setColor("Blanca");
+      System.out.println(miFurgoneta1.dameDatosFurgoneta());
    }
 }
